5e12586aa9790641bb6f8d362a6fc4b84eed2c71
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-01 10:59:18 +0530

    l

---
+++ script.js
@@ -0,0 +1,15 @@
+var num = 10;
+var str = "rajnish";
+var bool = true;
+var un = undefined;
+var nu = null;
+var nan = NaN;
+var obj = {};
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof un);
+console.log(typeof nu);
+console.log(typeof nan);
+console.log(typeof obj);
