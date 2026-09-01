33322d64b9782fd60f40d410a26af39d9fbf26bb
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-01 11:03:17 +0530

    l

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-var num = 10;
-var str = "rajnish";
-var bool = true;
-var un = undefined;
-var nu = null;
-var nan = NaN;
-var obj = {};
+// variable decleration
+var str = "Rajnish";
 
-console.log(typeof num);
-console.log(typeof str);
-console.log(typeof bool);
-console.log(typeof un);
-console.log(typeof nu);
-console.log(typeof nan);
-console.log(typeof obj);
+// variable define
+var name;
+user = "Rajnish";
