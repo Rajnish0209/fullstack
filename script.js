e32e980f209abc1c28f1e751446ab4e70edd436e
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-04 18:58:32 +0530

    l

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("12" - "12");
+let num = 12;
+let str = "rajnish";
+let bool = true;
+let nu = null;
+let un = undefined;
+let sym = Symbol("key");
+let obj = { user: "rajnish" };
+let arr = [1, 2, 3, 4];
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof nu);
+console.log(typeof un);
+console.log(typeof sym);
+console.log(typeof obj);
+console.log(typeof arr);
