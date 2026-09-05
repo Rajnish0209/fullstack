3382bc129aa0cc929512019c71035ab2ed87877a
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-05 11:53:23 +0530

    l

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ let sym = Symbol("key");
 let obj = { user: "rajnish" };
 let arr = [1, 2, 3, 4];
 
-console.log(typeof num);
-console.log(typeof str);
-console.log(typeof bool);
-console.log(typeof nu);
-console.log(typeof un);
-console.log(typeof sym);
-console.log(typeof obj);
-console.log(typeof arr);
+console.log(typeof num); //number
+console.log(typeof str); //string
+console.log(typeof bool); //boolean
+console.log(typeof nu); //object
+console.log(typeof un); //undefined
+console.log(typeof sym); // symbol
+console.log(typeof obj); //object
+console.log(typeof arr); //object
