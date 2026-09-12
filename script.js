93c9ace5419716da47aa7b559fd893177232cd51
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-12 19:48:37 +0530

    l

---
+++ script.js
@@ -0,0 +1,29 @@
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+const mernStack = ["MongoDB", "Express", "React", "Node"];
+
+for (let i = 0; i < countries.length; i++) {
+  console.log(countries[i]);
+}
