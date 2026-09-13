be3742af8ea6a4d618caa80d5697440585384b20
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-13 12:09:18 +0530

    l

--- script.js
+++ script.js
@@ -8,22 +8,21 @@ const countries = [
   "Germany",
   "Hungary",
   "Ireland",
+  "Ireland",
   "Japan",
   "Kenya",
 ];
 
-const webTechs = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "React",
-  "Redux",
-  "Node",
-  "MongoDB",
-];
-
-const mernStack = ["MongoDB", "Express", "React", "Node"];
+const newArr1 = [];
+const newArr2 = [];
 
 for (let i = 0; i < countries.length; i++) {
-  console.log(countries[i]);
+  if (countries[i].indexOf("land") != -1) {
+    newArr1.push(countries[i]);
+  } else {
+    newArr2.push(countries[i]);
+  }
 }
+
+console.log(newArr1);
+console.log(newArr2);
