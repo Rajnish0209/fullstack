1f65001b459ba9d4a8e8ccec1490216046cf8f1c
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-16 18:49:05 +0530

    l

---
+++ script.js
@@ -0,0 +1,28 @@
+function checkSeason(month) {
+  switch (month) {
+    case 1:
+    case 2:
+    case 12:
+      console.log("Winter");
+      break;
+    case 3:
+    case 4:
+    case 5:
+      console.log("spring");
+      break;
+    case 6:
+    case 7:
+    case 8:
+      console.log("summer");
+      break;
+    case 9:
+    case 10:
+    case 11:
+      console.log("autumn");
+      break;
+    default:
+      console.log("invalid");
+  }
+}
+
+checkSeason(5);
