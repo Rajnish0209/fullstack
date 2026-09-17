87c09e3fe87f84881e155c52c500da33b2043a75
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-17 13:48:29 +0530

    l

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function solveQuadEquation(a, b, c) {
-  let xCorrdinate = {};
-  xCorrdinate.x1 = ((-b + Math.sqrt(b ** 2 - 4 * a * c)) / 2) * a;
-  xCorrdinate.x1 = ((-b - Math.sqrt(b ** 2 - 4 * a * c)) / 2) * a;
-  return xCorrdinate;
-}
-
-console.log(solveQuadEquation(1, 4, 4));
+function capitalizeArray(arr) {}
