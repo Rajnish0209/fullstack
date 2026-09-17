0736521284564175d19e62da4e66c688dd0106cd
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-17 17:13:36 +0530

    l

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-function randomHexaNumberGenerator() {
-  let char = "0123456789abcdefghijklmnopqrstuvwxyz";
-  let randomHex = "";
-  for (let i = 0; i < 7; i++) {
-    randomHex += char[Math.floor(Math.random() * char.length)];
+function userIdGeneratedByUser() {
+  let char = "0123456789abcdefghiklmnopqrstuvwxyz";
+  let numberOfChar = prompt("Enter number of character");
+  let numberOfId = prompt("Enter number of id you want to generate");
+  if (numberOfChar <= 0 || numberOfId <= 0) {
+    return "Invalid input";
   }
-  return randomHex;
+  let arr = [];
+  for (let i = 0; i < numberOfId; i++) {
+    let id = "";
+    for (let j = 0; j < numberOfChar; j++) {
+      id += char[Math.floor(Math.random() * char.length)];
+    }
+    arr[i] = id;
+  }
+  return arr;
 }
 
-console.log(randomHexaNumberGenerator());
+console.log(userIdGeneratedByUser());
