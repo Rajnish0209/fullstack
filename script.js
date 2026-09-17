50bb1729d4e1145ca4f7585821b898c0a1cd290f
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-17 15:35:13 +0530

    l

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-function capitalizeArray(arr) {}
+function capitalizeArray(arr) {
+  let cArray = [];
+  for (let items of arr) {
+    cArray.push(items.toUpperCase());
+  }
+
+  return cArray;
+}
+
+console.log(capitalizeArray(["a", "b", "C"]));
