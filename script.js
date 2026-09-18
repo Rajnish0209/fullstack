2802981457ecaa5fed0cc49d6b1daaad39c57d97
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-18 19:26:27 +0530

    l

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-let a = 1;
-let b = 2;
-function print() {
-  console.log(a, b);
-  if (true) {
-    let a = 6;
-    let b = 9;
-    console.log(a, b);
-  }
-  console.log(a, b);
-}
+const person = {
+  firstName: "Rajnish",
+  lastName: "Gupta",
+  age: 26,
+  location: "Noida",
+  skills: ["HTML", "CSS", "JS", "React", "Node"],
+  isMarried: false,
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  "phone number": "+917703018388",
+  title: "teacher",
+};
+person.country = "India";
+person.getPersonInfo = function () {
+  let fullName = this.getFullName();
+  let skills = this.skills.slice(0, this.skills.length - 1).join(", ");
+  return `${fullName} is a ${this.title}. \nHe lives in ${this.location}. \nHe ${this.title} ${skills} and ${this.skills[this.skills.length - 1]}`;
+};
 
-print();
+console.log(person.hasOwnProperty("age"));
