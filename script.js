ba8501ca050e4cb64b56ce2a7dff0d3e134f4c96
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-19 14:21:19 +0530

    l

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 let personAccount = {
   firstName: "Rajnish",
   lastName: "Gupta",
-  balance: 0,
-  incomes: 0,
-  expenses: 0,
+  incomes: [
+    { description: "salary", amount: 4000 },
+    { description: "freelancer", amount: 800 },
+  ],
+  expenses: [
+    { description: "rent", amount: 1200 },
+    { description: "groceries", amount: 450 },
+  ],
   totalIncome: function () {
-    return this.incomes;
+    return this.incomes.reduce((sum, income) => sum + income.amount, 0);
   },
   totalExpense: function () {
-    return this.expenses;
+    return this.expenses.reduce((sum, expenses) => sum + expenses.amount, 0);
   },
   accountInfo: function () {
-    return `Name = ${this.firstName} ${this.lastName}\nTotal balance ${this.balance}`;
+    return `Name = ${this.firstName} ${this.lastName}\nTotal income ${this.totalIncome()}\nTotal expense ${this.totalExpense()}\nTotal balance ${this.accountBalance()}`;
   },
-  addIncome: function (amount) {
-    if (amount > 0) {
-      this.balance += amount;
-    } else {
-      return "invalid income";
-    }
+  addIncome: function (description, amount) {
+    this.incomes.push({ description: description, amount: amount });
   },
-  addExpense: function (amount) {
-    if (amount > 0) {
-      this.balance -= amount;
-    } else {
-      return "invalid expense";
-    }
+  addExpense: function (description, amount) {
+    this.expenses.push({ description: description, amount: amount });
   },
   accountBalance: function () {
-    return this.balance;
+    return this.totalIncome() - this.totalExpense();
   },
 };
 
-console.log(personAccount);
+personAccount.addExpense("cab", 500);
+console.log(personAccount.accountInfo());
