d378e91ab62c29ed1a78fb7cc7323bd96bcc21af
Rajnish0209 <rajnishkumargupta331@gmail.com>
2026-09-23 21:11:56 +0530

    l

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const set = new Set();
+const a = [4, 5, 8, 9];
+const b = [3, 4, 5, 7];
 
-console.log(ste);
+const A = new Set(a);
+const B = new Set(b);
+
+const AUnionB = new Set();
+
+for (let item of A) {
+  if (B.has(item)) AUnionB.add(item);
+}
+
+console.log(AUnionB);
